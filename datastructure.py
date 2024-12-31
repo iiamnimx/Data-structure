ee6b3e3b96eb8fe2f1eb88c8c10c035ac10dce89
iiamnimx <nimishaghosh402@gmail.com>
2024-12-31 19:35:59 +0530

    Add files via upload

---
+++ datastructure.py
@@ -0,0 +1,16 @@
+colorlist=["pink", "green", "yellow", "purple", "Orange","blue"]
+print(colorlist[3])
+
+print(colorlist[0:3])
+colorlist.remove("blue")
+print(colorlist)
+
+colorlist.pop(4)
+print(colorlist)
+
+print(len(colorlist))
+colorlist.insert(4,"beige")
+print(colorlist)
+
+colorlist.append("Black")
+print(colorlist)
